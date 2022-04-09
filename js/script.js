4165f7101ca61f787174b405c7b6784893ffcd19
Pablo Planas <planasboquer@gmail.com>
2022-04-08 21:07:04 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function burgerImage(){
+    document.getElementById('myImage').src='images/burger.jpeg';
+}
+
+function hotdogImage(){
+    document.getElementById('myImage').src='images/hotdog.jpeg';
+}
